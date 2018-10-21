612af217d3c2d53c8b2f6e54056c67df76452047
njaved <njaved@mit.edu>
2018-10-20 23:30:55 -0400

    fixed old map name reference in reformat_clumps function

--- map_building_functions.py
+++ map_building_functions.py
@@ -428,7 +428,7 @@ def reformat_clumps(chrom, int_directory):
                 block_dict[variant] = anchor
     anchor_file.close()
 
-    old_map = vcf.Reader(open(int_directory + chrom + ".common.recode.vcf", 'r'))
+    old_map = vcf.Reader(open(int_directory + "chr_" + chrom + ".common.recode.vcf", 'r'))
 
     new_map = open(int_directory + chrom + ".map", 'w')
     '''
